92c9300972e9141ebe16d5f82769dbe0dc46228c
rivatoDev <tomasrivara23.7@gmail.com>
2026-01-28 19:21:38 -0300

    directory error fixed

--- set-env.js
+++ set-env.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const targetPath = path.join(__dirname, './src/app/secrets/secret.ts'); 
+const targetPath = path.join(__dirname, './src/app/environments/secrets.ts'); 
 
 const envConfigFile = `
 export const secrets = {
   apiUrl: '${process.env.apiUrl}',
-  
+  mpPk: '${process.env.mpPk}'
 };
 `;
 
